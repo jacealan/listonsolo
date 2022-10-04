d0c2bf858820d1dce2125d402995f6ebab6e1d8e
Jace <jacealan1@gmail.com>
2022-10-05 03:22:41 +0900

    Let's start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from 'react';
+import ReactPlayer from 'react-player/lazy';
 
 function App() {
+  const [playUrl, setPlayUrl] = useState()
+  const playList = [
+    {title: '멜로망스 킬링보이스', url: 'https://youtu.be/hn4XiirKdNE'},
+    {title: '치즈 킬링보이스', url: 'https://youtu.be/fyDz91HDt4g'},
+    {title: '태연 킬링보이스', url: 'https://youtu.be/5ch94AaPZRQ'},
+    {title: '나윤권 킬링보이스', url: 'https://youtu.be/4w3p3ef2ydQ'},
+    {title: '벤 킬링보이스', url: 'https://youtu.be/4bwRyeT1afM'},
+  ]
+
+  const [info, setInfo] = useState({})
+  const getInfo = async (url) => {
+    const response = await fetch(`https://noembed.com/embed?dataType=json&url=${url}`)
+                      .then((response) => response.json());
+    setInfo(response)
+  }
+  useEffect(() => {
+    getInfo('https://youtu.be/fyDz91HDt4g')
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo">ListOn</div>
+      <div className="list-title">the Playlist</div>
+      
+      <div className="play-window">
+        <ReactPlayer url='https://youtu.be/5ch94AaPZRQ' width="960px" height="480px" />        
+      </div>
+      <div className="play-info">
+        {info.title}
+      </div>
+
+      <div className="add-list">
+
+      </div>
+
+      <div classname="play-list">
+        { playList.map(({title, url}, index) => (
+          <div>
+            <ReactPlayer url={url} width="180px" height="auto" />
+            {index} {title} {url}
+          </div>
+        ))}
+      </div>
+      <style jsx>{`
+        .App {
+          width: 100vw;
+          max-width: 960px;
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-template-rows: 
+        }
+
+        .logo {
+          grid-area: 1 / 1 / 2 / 3;
+        }
+
+        .list-title {          
+          grid-area: 2 / 1 / 3 / 3;
+        }
+
+        .play-window {
+          grid-area: 3 / 1 / 4 / 3;
+        }
+
+        .play-info {
+          grid-area: 4 / 1 / 5 / 3;
+
+        }
+        .add-list {
+          grid-area: 5 / 1 / 6 / 2;
+        }
+
+        .play-list {
+          grid-area: 5 / 2 / 6 / 3;
+        }
+      `}</style>
     </div>
   );
 }
