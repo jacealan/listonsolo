62a1df64c9a8df4c8e70e61422f5db6b48f82dae
Jace <jacealan1@gmail.com>
2022-10-05 11:24:15 +0900

    Modify resize code

--- src/App.js
+++ src/App.js
@@ -34,37 +34,9 @@ function App() {
     })
   }
   useEffect(() => {
-    // const x960 = window.innerWidth < 960 ? window.innerWidth : 960
-    // setWindowSize({
-    //   x: x960,
-    //   y: window.innerHeight,
-    //   xPlay: x960,
-    //   yPlay: parseInt(x960 / 2),
-    //   xHalf: parseInt(x960 / 2),
-    // })
     changeSizeTotal()
     window.addEventListener('resize', changeSizeTotal);
     window.addEventListener('orientationchange', changeSizeTotal);
-    // window.addEventListener('resize', () => {
-    //   const x960 = window.innerWidth < 960 ? window.innerWidth : 960
-    //   setWindowSize({
-    //     x: x960,
-    //     y: window.innerHeight,
-    //     xPlay: x960,
-    //     yPlay: parseInt(x960 / 2),
-    //     xHalf: parseInt(x960 / 2),
-    //   })
-    // });
-    // window.addEventListener('orientationchange', () => {
-    //   const x960 = window.innerWidth < 960 ? window.innerWidth : 960
-    //   setWindowSize({
-    //     x: x960,
-    //     y: window.innerHeight,
-    //     xPlay: x960,
-    //     yPlay: parseInt(x960 / 2),
-    //     xHalf: parseInt(x960 / 2),
-    //   })
-    // });
   }, [])
   
   return (
